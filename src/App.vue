9f118993722855b278b0842a290c5fa5bcae6cd3
zoe2276 <zoe22761@gmail.com>
2025-09-13 14:09:57 -0500

    commit, innit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="appRoot">
+    <h1>{{ title }}</h1>
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  name: "App",
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      title: "a Vue app"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app {
+  font-family: "Arial sans-serif";
+  padding: 20px;
+}
+
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button @click="count++">
+            Clicks: {{ count }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Counter",
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    padding: 10px 20px;
+    background-color: #42b983;
+    color: white;
+    border: none;
+    border-radius: 8px;
+
+    will-change: filter;
+    transition: filter 137ms cubic-bezier(0.5, 0, 1, 2);
+
+    &:hover {
+        filter: drop-shadow(0 0 2.9vmin #42b98377);
+    }
+}
+</style>
