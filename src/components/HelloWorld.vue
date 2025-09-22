5cf91986eef4ef4ae63551d91bc0c9f3d091ec3b
zoe2276 <zoe22761@gmail.com>
2025-09-22 14:58:50 -0500

    undo demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,12 +19,12 @@ const count = ref(0)
     </p>
   </div>
 
-  <!-- <p>
+  <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
-  </p> -->
+  </p>
   <p>
     Learn more about IDE Support for Vue in the
     <a
