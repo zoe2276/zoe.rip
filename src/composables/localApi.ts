b1f368d68ec70ba54be7b6086aa5228cb589ed18
zoe2276 <zoe22761@gmail.com>
2025-12-23 21:15:21 -0600

    whoops i pr too early

--- src/composables/localApi.ts
+++ src/composables/localApi.ts
@@ -6,11 +6,12 @@ export interface ApiParameters {
 
 async function callApi<T>(endpoint: string, params: ApiParameters): Promise<T> {
     params.headers ? params.headers.set("Content-Type", "application/json") : params.headers = new Headers({"Content-Type": "application/json"})
-    const res = await fetch(`${import.meta.env.VITE_BASE_URL || "http://localhost:8099"}${endpoint}`, params)
+    const res = await fetch(`${"https://auth.zoe.rip" /* import.meta.env.VITE_BASE_URL || "http://localhost:8099" */}${endpoint}`, params)
 
     if (!res.ok) throw res
-    const js =  setTimeout(async () => await res.json(), 100) as T
-    return js // adds some frontend latency to slow down call frequency
+    setTimeout(() => {}, 100) // frontend slowdown
+    const js = await res.json() as T
+    return js
 }
 
 export const register = async (username: string, email: string, password: string, role: string = "user") => {
@@ -33,7 +34,7 @@ export const login = async (username: string, password: string) => {
         password: password
     }
 
-    return await callApi<{ token: string, expiration: string}>("/api/token", {
+    return await callApi<{ token: string, expires: string}>("/api/token", {
         method: "post",
         body: JSON.stringify(credsObj)
     })
